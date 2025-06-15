0ac445e4a91470f6619516d895e1b55d03a5e970
Howard-12 <s0989175085@gmail.com>
2025-06-15 15:46:22 +1000

    update: import more files

--- zigood.zig
+++ zigood.zig
@@ -1,7 +1,11 @@
+pub const util = @import("src/utils.zig");
+
 pub const xor = @import("src/encryption/xor.zig");
 pub const rc41 = @import("src/encryption/rc4.zig");
 pub const rc42 = @import("src/encryption/rc42.zig");
 pub const aes = @import("src/encryption/aes.zig");
+
+pub const ipv4 = @import("src/obfuscation/ivp4.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
