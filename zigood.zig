fcd18de292b63de481b97fd19f65ae76b2a9619d
Howard-12 <s0989175085@gmail.com>
2025-06-18 00:20:07 +1000

    fix: file name

--- zigood.zig
+++ zigood.zig
@@ -5,7 +5,7 @@ pub const rc41 = @import("src/encryption/rc4.zig");
 pub const rc42 = @import("src/encryption/rc42.zig");
 pub const aes = @import("src/encryption/aes.zig");
 
-pub const ipv4 = @import("src/obfuscation/ivp4.zig");
+pub const ipv4 = @import("src/obfuscation/ipv4.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
